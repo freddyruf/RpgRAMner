e4985e8c7aeb4e795b662051a4ee2fff8a68e0cc
Federico Ruffini <federico01.ruffini@studenti.unicam.it>
2026-06-05 16:34:31 +0200

    Initial commit: è stato fatto il setup del progetto, con gradle, javaFX e un piccolo esempio di test

---
+++ app/src/main/java/it/unicam/cs/mpgc/rpg130077/App.java
@@ -0,0 +1,29 @@
+
+package it.unicam.cs.mpgc.rpg130077;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class App extends Application {
+    @Override
+    public void start(Stage stage) {
+        Parent root = null;
+        try { //se esiste apro il file.fxlm
+            root = FXMLLoader.load(App.class.getResource("/it/unicam/cs/mpgc/rpg130077/Untitled.fxml"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        //imposto la scena e la visualizzo
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
